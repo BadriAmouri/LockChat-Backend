bdd2bf65471d1686532a2fac343c57dda36b3a94
fatima-zahra-bourouba <fatima.zahra.bourouba@ensia.edu.dz>
2025-03-30 13:21:56 +0100

    Generate Keys using ECC

--- app.js
+++ app.js
@@ -1,19 +1,10 @@
 const express = require('express');
 const supabase = require('./src/config/db');
+const authRoutes = require('./src/routes/authRoutes');
 
 const app = express();
-
-app.get('/test-db', async (req, res) => {
-    console.log("Testing Supabase connection...");
-
-    const { data, error } = await supabase.from('users').select('*'); // Fetch users
-
-    if (error) {
-        return res.status(500).json({ error: error.message });
-    }
-
-    res.json({ users: data });
-});
+app.use(express.json()); // Middleware to parse JSON bodies
+app.use('/auth', authRoutes); // Use the auth routes
 
 const PORT = 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
