6d59722a759dd1190050c937cd1e139a0a0e8449
fatima-zahra-bourouba <fatima.zahra.bourouba@ensia.edu.dz>
2025-03-30 21:03:38 +0100

    login implementation backend

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -58,4 +58,50 @@ exports.register = async (req, res) => {
         res.status(500).json({ error: "Error in signing the user up" });
     }
 }
+exports.login = async (req, res) => {
+    try {
+        //getting user data from the body 
+        const {username, password} = req.body;
 
+        //Check if all the fields are filled
+        if (!username || !password) {
+            return res.status(400).json({ error: "All fields are required" });
+        }
+
+        //fetch data from the data base 
+        const userData = await user.getUserByUsername(username);
+        console.log ("user data from the database: ");
+        console.log(await user.getUserByUsername(username));
+        //Check if the user exists  
+        if (!userData) {
+            return res.status(400).json({ error: "User not found" });
+        }
+
+        //Check if the password matches 
+        const isCorrectPW = await bcrypt.compare (password, userData.password_hash);
+        if (!isCorrectPW) {
+            return res.status(400).json({ error: "Invalid password" });
+        }
+
+        const token = jwt.sign(
+            {
+                user_id : userData.user_id,
+                username : userData.username,
+                email : userData.email,
+                publicKey : userData.public_key,
+            },
+            process.env.JWT_SECRECT_KEY,
+            {
+                expiresIn: "24h",
+            }
+        );
+        //Return the token and the user data with the private key to be stored on the user device
+        res.status(200).json({
+            message:"user logged in successfully",
+            token: token,
+        });
+    }catch (error) {
+        console.error("Error logging in user:", error);
+        res.status(500).json({ error: "Error logging in user" });
+    }
+}
