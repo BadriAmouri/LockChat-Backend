46d605353e9370c699739712a61cc7afd4890d2c
Amouribadri <badriamouri2016@gmail.com>
2025-04-29 15:52:43 +0100

    added_check_connection_api

--- src/routes/webSocketRoute.js
+++ src/routes/webSocketRoute.js
@@ -2,7 +2,15 @@
 const express = require('express');
 const router = express.Router();
 const { handleSocketId } = require('../controllers/webSocketController');
+const { checkUserConnection } = require('../websocket/websocket');
+
 
 router.post('/get-socket-id', handleSocketId);
+
+router.post('/check-user-connection', (req, res) => {
+    const { userId } = req.body;
+    const isConnected = checkUserConnection(userId);
+    res.status(200).json({ isConnected });
+  });
 
 module.exports = router;
