84080edee693dcf53e11f34b2d08bbe43ec0b19a
Abderrahmene Tliba <abderrahmene.tliba@ensia.edu.dz>
2025-04-29 20:13:16 +0100

    requests search

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -5,7 +5,7 @@ const UserModel = require('../models/User');
 class UserController{
 static async searchForUsers (req, res) {
   const { query } = req.query;
-  const userId = req.user.user_id
+  const currentUserId = req.user.user_id
 
   if (!query) {
     return res.status(400).json({ message: 'Query is required' });
